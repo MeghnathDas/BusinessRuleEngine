378c06bf1f6a3fb6edb687a2fc964419b48e6a58
Meghnath Das <meghnath.babin@gmail.com>
2020-05-28 11:03:48 +0530

    Initial commit

---
+++ MD.BusinessRuleEngine/IRuleEngine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD.BusinessRuleEngine
+{
+    public interface IRuleEngine
+    {
+        public Task GetTask();
+    }
+}
---
+++ MD.BusinessRuleEngine/PaymentRuleProcessorEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD.BusinessRuleEngine
+{
+    public abstract class PaymentRuleProcessorEngine : IRuleEngine
+    {
+        public abstract void GetPaymentData();
+
+        public Task GetTask()
+        {
+            return null;
+        }
+    }
+}
