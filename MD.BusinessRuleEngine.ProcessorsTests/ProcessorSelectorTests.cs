c0884011de5429e3f1fba5ab36336740837fd16e
Meghnath Das <meghnath.babin@gmail.com>
2020-05-28 11:54:43 +0530

    Basic test case added for processor selector

---
+++ MD.BusinessRuleEngine.ProcessorsTests/ProcessorSelectorTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MD.BusinessRuleEngine.Processors.Extension;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MD.BusinessRuleEngine.Models;
+
+namespace MD.BusinessRuleEngine.Processors.Extension.Tests
+{
+    [TestClass()]
+    public class ProcessorSelectorTests
+    {
+        [TestMethod()]
+        public void GetProcessorTest()
+        {
+            var paymentData = new CustomerPayment
+            {
+                Amount = 10,
+                CustomerName = "Test Customer 1",
+                Description = "Test Description 1",
+                ForProductType = ProductType.Book
+            };
+
+            var bookProcessor = paymentData.GetProcessor();
+            Assert.IsNotNull(bookProcessor);
+        }
+    }
+}
